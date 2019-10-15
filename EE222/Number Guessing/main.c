9292d1b85af9ee3f0bfda99db4134eab18abe86a
Kaelen Carling <kaelen.carling@gmail.com>
2019-10-15 01:00:10 -0700

    number guessing
    number guessing project

--- EE222/Number Guessing/main.c
+++ EE222/Number Guessing/main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
+#define MAX_NUMBER 1000
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
-{
-	int min = 0;
-	int max = 1000;
-	int userNum;
-	bool correct = false;
-	do
-	{
-		printf("please enter a number between %d and %d", min, max);
-		scanf("%d", userNum);
-		
-	}while(!correct)
+int main(int argc, char *argv[]) {
 	
+	int count = 1;
+	int guess = 0;
+	int min = 1;
+	int max = MAX_NUMBER;
+	char inChar;
+	
+	while(max != min)
+	{
+		guess = (min + max)/2;
+		printf("%i. Is your number greater than %i? (y/n)", count, guess);
+		scanf("%c", &inChar);
+		getchar();
+		//printf("%c", inChar);
+		
+		if(inChar == 'y' || inChar == 'Y')
+		{
+			min = guess;
+		}
+		else
+		{
+			max = guess;
+		}
+		count++;
+	}
+	printf("Your number is: %i", guess);
 	return 0;
 }
